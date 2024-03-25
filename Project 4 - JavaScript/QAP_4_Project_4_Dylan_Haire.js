02f6d47a55ac797eaa2592d9be26c872f6ba12ca
dhaire-keyin <162446915+dhaire-keyin@users.noreply.github.com>
2024-03-24 23:49:36 -0230

    Add files via upload

---
+++ Project 4 - JavaScript/QAP_4_Project_4_Dylan_Haire.js
@@ -0,0 +1,15 @@
+const custMotel = {
+	// set as array
+	custName = "John Doe";
+	dateCustBirth = "1986-04-26";
+	custGender = "Male";
+	custRoomPref = "Single Bed";
+	// set as sub-object 1
+	custPayMeth = "Credit Card";
+	custAddrMail = "23 Higgins Line, St. John's, NL, A1B 4J6";
+	// set as sub-object 2
+	custPhone = "709-555-5555";
+	dateCheckIn = "2024-03-24";
+	dateCheckOut = "2024-04-01";
+	// object methods
+}
