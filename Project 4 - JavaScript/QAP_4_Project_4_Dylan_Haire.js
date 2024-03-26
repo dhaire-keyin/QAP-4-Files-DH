32741780a28a76bdaaa651d3f7736df42354bedc
dhaire-keyin <162446915+dhaire-keyin@users.noreply.github.com>
2024-03-26 19:13:41 -0230

    Add files via upload

--- Project 4 - JavaScript/QAP_4_Project_4_Dylan_Haire.js
+++ Project 4 - JavaScript/QAP_4_Project_4_Dylan_Haire.js
@@ -1,15 +1,32 @@
 const custMotel = {
 	// set as array
-	custName = "John Doe";
-	dateCustBirth = "1986-04-26";
-	custGender = "Male";
-	custRoomPref = "Single Bed";
+	cust_data: ["John Doe", "1986-04-26", "Male", "Single Bed"],
 	// set as sub-object 1
-	custPayMeth = "Credit Card";
-	custAddrMail = "23 Higgins Line, St. John's, NL, A1B 4J6";
+	paymentInfo: {
+		custPayMeth: "Credit Card",
+		custAddrMail: "23 Higgins Line, St. John's, NL, A1B 4J6",
+	},
 	// set as sub-object 2
-	custPhone = "709-555-5555";
-	dateCheckIn = "2024-03-24";
-	dateCheckOut = "2024-04-01";
+	bookingInfo: {
+		custPhone: "709-555-5555",
+		dateCheckIn: "2024-03-24",
+		dateCheckOut: "2024-04-01",
+        numRoom: "426"
+	},
+	
 	// object methods
+	// Customer age
+	dateToday: new Date(),
+	getCustAge: function() {
+
+		return
+	},
+	// Duration of stay
+	getCustStay: function() {
+		// Check-out date - check-in date
+		return
+	},
+
+	// Template string - customer description
+	custDescr: `${cust_data[0]} is a ${getCustAge()}-year old ${cust_data[2]}`
 }
