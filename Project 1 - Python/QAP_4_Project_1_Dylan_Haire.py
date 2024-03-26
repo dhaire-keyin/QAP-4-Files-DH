8cafc926ac9c6aceeea0e952b5560395a6875f99
dhaire-keyin <162446915+dhaire-keyin@users.noreply.github.com>
2024-03-26 19:14:43 -0230

    Add files via upload

---
+++ Project 1 - Python/QAP_4_Project_1_Dylan_Haire.py
@@ -0,0 +1,20 @@
+# Description: A program for the One Stop Insurance Company to enter and calculate new insurance policy information for its customers
+# Author: Dylan Haire
+# Date(s): March 25, 2024
+
+# Import libraries
+
+# Define program constants
+provLst = ["AB","BC","MB","NB","NL","NS","NT","NU","ON","PE","QC","SK","YT"]
+payMethLst = ["Full","Monthly","Down Payment"]
+# Define program functions
+
+# Main program
+while True:
+    # Gather user input
+
+    # Perform required calculations
+
+    # Display results
+	break
+# Any housekeeping duties at the end of the program
